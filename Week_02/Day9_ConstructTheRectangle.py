c0c7e98ceb2b86b364a87d0a0cddb8e0643bb81f
Shiv Narayan Prasad <shivnarayanprasad2007@gmail.com>
2026-06-10 22:42:59 +0530

    Day 9: Solved Construct the Rectangle

---
+++ Week_02/Day9_ConstructTheRectangle.py
@@ -0,0 +1,8 @@
+import math
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+        w = int(math.sqrt(area))
+        while area % w != 0:
+            w-=1
+        l = area//w
+        return [l,w]
