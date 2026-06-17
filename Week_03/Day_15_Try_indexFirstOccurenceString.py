6170139993032b014594586f579c60e99015a9a7
Shiv Narayan Prasad <shivnarayanprasad2007@gmail.com>
2026-06-18 00:12:29 +0530

    Day 15: Solved Find the index of First occurence in a String using Try catch

---
+++ Week_03/Day_15_Try_indexFirstOccurenceString.py
@@ -0,0 +1,8 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        try:
+            return haystack.index(needle)
+        
+        except ValueError :
+            return -1
+        
